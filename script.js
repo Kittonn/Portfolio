4274d05f32ce00362f15def66db12f6888a1a5fa
Kitton._ <tonkittipod@gmail.com>
2021-12-16 12:21:48 +0700

    .

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.right = "-200px";
 }
+
+$(document).ready(function(){
+    var typed = new Typed(".typing",{
+        strings: ["KITTIPOD LAMBANGCHANG","TON","KITTON._","LNWTXN"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+
+    var typed2 = new Typed(".type",{
+        strings: ["Data Science", "Artificial Intelligent", "Block Chain"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+});
